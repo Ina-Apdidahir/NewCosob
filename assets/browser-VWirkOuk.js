b25f53164f56562fe768f50ab7accbf704a505ce
Ina-Apdidahir <150722233+Ina-Apdidahir@users.noreply.github.com>
2024-08-11 22:34:21 +0300

    Updates

--- assets/browser-VWirkOuk.js
+++ assets/browser-VWirkOuk.js
@@ -1,4 +1,4 @@
-import{c as Pe,g as Xe}from"./index-BNvFsBfn.js";var se={exports:{}};/** @license
+import{c as Pe,g as Xe}from"./index-BVMx-ask.js";var se={exports:{}};/** @license
  * eventsource.js
  * Available under MIT License (MIT)
  * https://github.com/Yaffle/EventSource/
